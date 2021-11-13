a5c0a9a242e449e3de1426b95916275a4d340985
KronsyC <87078913+KronsyC@users.noreply.github.com>
2021-11-13 16:17:55 +0000

    Calculator now remembers previous answer and manipulates it if next input is an operator

--- index.js
+++ index.js
@@ -1,88 +1,89 @@
-
 // Numbers
-const num1 = document.getElementById("1")
-const num2 = document.getElementById("2")
-const num3 = document.getElementById("3")
-const num4 = document.getElementById("4")
-const num5 = document.getElementById("5")
-const num6 = document.getElementById("6")
-const num7 = document.getElementById("7")
-const num8 = document.getElementById("8")
-const num9 = document.getElementById("9")
-const num0 = document.getElementById("0")
+const num1 = document.getElementById("1");
+const num2 = document.getElementById("2");
+const num3 = document.getElementById("3");
+const num4 = document.getElementById("4");
+const num5 = document.getElementById("5");
+const num6 = document.getElementById("6");
+const num7 = document.getElementById("7");
+const num8 = document.getElementById("8");
+const num9 = document.getElementById("9");
+const num0 = document.getElementById("0");
 
 // Operators
-const equ = document.getElementById("=")
-const plus = document.getElementById("+")
-const minus = document.getElementById("-")
-const div = document.getElementById("/")
-const mul = document.getElementById("*")
+const equ = document.getElementById("=");
+const plus = document.getElementById("+");
+const minus = document.getElementById("-");
+const div = document.getElementById("/");
+const mul = document.getElementById("*");
 
-const currentScreen = document.getElementById("currentsum")
-const history = document.getElementById("history")
+const currentScreen = document.getElementById("currentsum");
+const history = document.getElementById("history");
 
-var equString = ""
+var equString = "";
+var prev = "";
+function addNum(num) {
+  equString += num;
+
+  currentScreen.innerHTML = equString;
+}
+function addOperand(op){
+    if(prev != ""){
+        equString+=prev
+        prev=""
+    }
+    equString+=op
+    currentScreen.innerHTML = equString;
+}
 
 num1.onclick = () => {
-    equString += "1"
-    currentScreen.innerHTML = equString
-}
+  addNum(1);
+};
 num2.onclick = () => {
-    equString += "2"
-    currentScreen.innerHTML = equString
-}
+  addNum(2);
+};
 num3.onclick = () => {
-    equString += "3"
-    currentScreen.innerHTML = equString
-}
+  addNum(3);
+};
 num4.onclick = () => {
-    equString += "4"
-    currentScreen.innerHTML = equString
-}
+  addNum(4);
+};
 num5.onclick = () => {
-    equString += "5"
-    currentScreen.innerHTML = equString
-}
+  addNum(5);
+};
 num6.onclick = () => {
-    equString += "6"
-    currentScreen.innerHTML = equString
-}
+  addNum(6);
+};
 num7.onclick = () => {
-    equString += "7"
-    currentScreen.innerHTML = equString
-}
+  addNum(7);
+};
 num8.onclick = () => {
-    equString += "8"
-    currentScreen.innerHTML = equString
-}
+  addNum(8);
+};
 num9.onclick = () => {
-    equString += "9"
-    currentScreen.innerHTML = equString
-}
+  addNum(9);
+};
 num0.onclick = () => {
-    equString += "0"
-    currentScreen.innerHTML = equString
-}
+  addNum(0);
+};
 plus.onclick = () => {
-    equString += "+"
-    currentScreen.innerHTML = equString
-}
+  addOperand("+")
+};
 minus.onclick = () => {
-    equString += "-"
-    currentScreen.innerHTML = equString
-}
+    addOperand("-")
+};
 div.onclick = () => {
-    equString += "/"
-    currentScreen.innerHTML = equString
-}
+    addOperand("/")
+};
 mul.onclick = () => {
-    equString += "*"
-    currentScreen.innerHTML = equString
-}
+    addOperand("*")
+
+};
 
 equ.onclick = () => {
-    const answer = eval(equString)
-    currentScreen.innerHTML = answer
-    history.innerHTML = equString
-    equString = ""
-}
+  const answer = eval(equString);
+  prev = answer
+  currentScreen.innerHTML = answer;
+  history.innerHTML = equString;
+  equString = "";
+};
